6be6006dd5f939c902b5fa49125171f0cf6db293
kst <kst@ksts-Mac-mini.local>
2022-12-04 16:49:35 +0900

    fixed

--- src/logic.js
+++ src/logic.js
@@ -1,83 +1,48 @@
 const kokac = new KokacP5Ext();
-
-let myFont;
-function preload() {
-  myFont = loadFont("fonts/NotoSansKR-Bold.otf");
-}
-async function setup() {
-  // 로직은 setup 안에서만 손 대면 되도록 코드를 구성함
-  let sizeWidth = document.querySelector("#stg").getBoundingClientRect().width;
-  let sizeHeight = sizeWidth * 0.3;
-  sizeWidth = innerWidth;
-  sizeHeight = innerHeight;
-  createCanvas(sizeWidth, sizeHeight);
+function setup() {
+  createCanvas(innerWidth, innerHeight);
+  kokac.add(() => background(0, 0, 0, 10));
   noStroke();
-  kokac.add((e) => background(0));
-  kokac.add((e) => {
-    fill("#00dd00");
-    kokac.text(`abc\ndef\n한글`, 10, 0, 230, myFont);
-  });
-  //-----------------------------------------
-  class Mother {
-    constructor() {
-      this.x = 0;
-      this.y = 0;
-      this.r = 20;
-      this.alpha = 0;
-      this.alphaToggle = false;
-    }
-    update() {
-      this.x += 0.1;
-      if (!this.alphaToggle) {
-        this.alpha++;
-        if (this.alpha >= 255) this.alphaToggle = !this.alphaToggle;
-      } else {
-        this.alpha--;
-        if (this.alpha <= 0) this.alphaToggle = !this.alphaToggle;
-      }
-    }
-    redraw() {
-      fill(255, 0, 0, this.alpha);
-      circle(this.x, this.y, this.r * 2);
-    }
-  }
-  for (let i = 0; i < 10; i++) {
-    let c2 = new Mother();
-    c2.x = c2.r;
-    c2.y = c2.r + i * c2.r * 2;
-    kokac.add(c2);
-    let c3 = { r: 10 };
-    c3.redraw = function () {
-      fill(255, 255, 0);
-      circle(c2.x, c2.y, this.r * 2);
-    };
-    kokac.add(c3);
 
-    kokac.add(() => {
-      let bound = kokac.textBounds(
-        `${Math.random()}`,
-        c2.x,
-        c2.y,
-        20,
-        myFont,
-        true
-      );
-      let margin = 4;
-      push();
-      stroke(255);
-      fill(30);
-      rect(
-        c2.r + margin + bound.rect.x - margin,
-        bound.rect.y - margin - bound.rect.h * 0.5,
-        bound.rect.w + margin * 2,
-        bound.rect.h + margin * 2
-      );
-      pop();
-      let { string, x, y, font, size } = bound;
-      fill(255);
-      textFont(font);
-      textSize(size);
-      text(string, c2.r + margin + x, y - bound.rect.h * 0.5);
-    });
+  function makeThread() {
+    let dot = {
+      x: mouseX,
+      y: mouseY,
+      color: color(random(200, 255), random(200, 255), random(200, 255)),
+      dir: -QUARTER_PI * 2,
+      setDir() {
+        const div = 0.5;
+        this.from = { x: this.x, y: this.y };
+        this.dir += QUARTER_PI * div * (Math.random() < 0.5 ? -1 : 1);
+        this.length = 0;
+      },
+      update() {
+        if (
+          this.x < 0 ||
+          this.y < 0 ||
+          this.x > innerWidth ||
+          this.y > innerHeight
+        ) {
+          kokac.remove(dot);
+          makeThread();
+        }
+        if (!this.length || this.length === lineLength) this.setDir();
+        this.length += innerWidth / 200;
+        if (this.length >= lineLength) this.length = lineLength;
+        const { x, y } = this.from;
+        const pos = Geometry.getPointByRadian(x, y, this.dir, this.length);
+        this.x = pos[0];
+        this.y = pos[1];
+      },
+      redraw() {
+        fill(this.color);
+        circle(this.x, this.y, innerWidth / 500);
+      },
+    };
+    kokac.add(dot);
+  }
+  const lineLength = innerWidth / 200;
+  for (let i = 0; i < 5000; i++) {
+    makeThread();
   }
 }
